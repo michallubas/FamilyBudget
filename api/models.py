882850a2417a41c328d8fd177548e0abdfeaccb5
@lubczyk79 <mlubas@infracert.com>
2021-04-15 23:07:23 +0200

    Added validators to model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
 
 class Budget(models.Model):
@@ -10,7 +10,17 @@ class Budget(models.Model):
     def __str__(self):
         return f"Budget {self.name}, its {self.description} "
 
+
+tempCategory = RegexValidator('home|shopping|food|vehicles|vacation|school|communication',
+                              'Only: home, shopping, food, vehicles, vacation, school, communication categories allowed')
+
+
 class InOut(models.Model):
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(decimal_places=2, max_digits=8, validators=[MinValueValidator(100), MaxValueValidator(100)])
+    category = models.CharField(max_length=16, validators=[tempCategory])
+    amount = models.DecimalField(decimal_places=2, max_digits=8,
+                                 validators=[MinValueValidator(100), MaxValueValidator(100)])
+
+    def __str__(self):
+        return f"Income/expenses for category {self.category} "
